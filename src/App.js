3d9ff99550eb3e792cd998fee0f27c5086600700
CamillaWan <camillawan99@gmail.com>
2025-03-25 16:26:55 +1000

    apply tailwind style to background and make it show properly

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import BackgroundImage from './components/BackgroundImage';
 import WeatherCard from './components/WeatherCard';
-import bg from './assets/bg.png';
+
 
 const App = () => (
-  <BackgroundImage imageUrl={bg} alt="cloud" className="w-screen h-screen">
+  <BackgroundImage>
    <WeatherCard />
   </BackgroundImage>
 )
