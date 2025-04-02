7d7394b8419015920bad5ec67e873241c20f0cfb
CamillaWan <camillawan99@gmail.com>
2025-04-02 23:48:35 +1000

    add content to OtherCities component

--- src/components/WeatherCard/components/OtherCities/OtherCities.js
+++ src/components/WeatherCard/components/OtherCities/OtherCities.js
@@ -1,11 +1,15 @@
 import City from "./Components/City/City";
+import sunny from '../../../../assets/weather_icon/Sunny.png';
+import cloudy from '../../../../assets/weather_icon/Cloudy.png';        
+import rain from '../../../../assets/weather_icon/Rain.png';
+import snow from '../../../../assets/weather_icon/Snow.png';
 
 const OtherCities = () => (
-    <div className="flex">
-        <City   />
-        <City   />
-        <City   />
-        <City   />
+    <div className='flex justify-between text-white'>
+        <City className='bg-Sydney' imageUrl={sunny} name='Sydney' temperatureRange='28~32'/>
+        <City className='bg-Shanghai' imageUrl={cloudy} name='Shanghai' temperatureRange='20~23'/>
+        <City className='bg-Newyork' imageUrl={rain} name='New York' temperatureRange='20~28'/>
+        <City className='bg-London' imageUrl={snow} name='London' temperatureRange='0~2'/>
     </div>
 )
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,12 @@ module.exports = {
       backgroundImage: {
       'gradient-to-l': 'linear-gradient(to left,rgb(73, 93, 245),rgb(145, 155, 251))',
       'gradient-to-tl': 'linear-gradient(to top left,rgb(56, 82, 250),rgb(145, 162, 252))',
+      'Sydney':'url(../src/assets/City/Sydney.png)',
+      'Shanghai':'url(../src/assets/City/Shanghai.png)',
+      'Newyork':'url(../src/assets/City/Newyork.png)',
+      'London':'url(../src/assets/City/London.png)',
       },
+
       backgroundPosition: {
         'bottom-right': 'bottom right',
       },
