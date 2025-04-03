fca96404c3632051e0cb963fdb07751ba8d6681e
CamillaWan <camillawan99@gmail.com>
2025-04-03 23:36:45 +1000

    improve OtherCities component

--- src/components/WeatherCard/components/OtherCities/Components/City/City.js
+++ src/components/WeatherCard/components/OtherCities/Components/City/City.js
@@ -5,7 +5,7 @@ import WeatherIcon from "../../../../../WeatherIcon";
 const City = ({className, imageUrl, name, temperatureRange}) => {
     return ( 
         <div className={`${className} bg-cover bg-no-repeat bg-center m-4 w-full h-full rounded-3xl flex flex-col justify-center items-center shadow-lg`}>
-            <div className='bg-gradient-to-l opacity-70 bg-cover'>
+            <div className='bg-gradient-to-l opacity-70 bg-cover rounded-3xl'>
             <WeatherIcon imageUrl={imageUrl}/>
             <Name value={name} />
             <TemperatureRange value={temperatureRange} />
