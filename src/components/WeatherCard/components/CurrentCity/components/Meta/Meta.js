81c0cfb56f07be3457247422b8533d662aa47183
CamillaWan <camillawan99@gmail.com>
2025-04-07 01:01:59 +1000

    adjust layout for CurrentCity and OtherCities components

--- src/components/WeatherCard/components/CurrentCity/components/Meta/Meta.js
+++ src/components/WeatherCard/components/CurrentCity/components/Meta/Meta.js
@@ -6,7 +6,7 @@ import Somatosensory from '../../../../../../../src/assets/meta_icon/Somatosenso
 
 const Meta = () => {
     return ( 
-        <div className='flex justify-between bg-slate rounded-2xl w-5/6 p-4'>
+        <div className='flex justify-between bg-slate rounded-2xl w-full p-4 overflow-hidden'>
             <MetaItem imageUrl={Humidity} value='85%' alt='Humidity percentage' />
             <MetaItem imageUrl={wind} value='9 km/h' alt='Wind speed'  />
             <MetaItem imageUrl={AirQuality} value='75 μg/m³' alt='PM2.5' />
