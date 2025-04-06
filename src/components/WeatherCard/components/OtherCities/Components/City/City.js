230751de267a051144592daa7dbf52bb48d1fdb4
CamillaWan <camillawan99@gmail.com>
2025-04-06 22:56:22 +1000

    improve responsive performance for OtherCities components

--- src/components/WeatherCard/components/OtherCities/Components/City/City.js
+++ src/components/WeatherCard/components/OtherCities/Components/City/City.js
@@ -4,9 +4,9 @@ import WeatherIcon from "../../../../../WeatherIcon";
 
 const City = ({className, imageUrl, name, temperatureRange}) => {
     return ( 
-        <div className={`${className} h-full bg-cover bg-no-repeat bg-center mx-2 rounded-3xl flex flex-col justify-center items-center shadow-lg`}>
-            <div className='bg-gradient-to-l opacity-70 bg-cover h-full rounded-3xl'>
-                <WeatherIcon className='px-12' imageUrl={imageUrl}/>
+        <div className={`${className} h-full w-full bg-cover bg-no-repeat bg-center mx-4 rounded-3xl flex flex-col justify-center items-center shadow-lg`}>
+            <div className='flex flex-col items-center overflow-hidden bg-gradient-to-l opacity-70 bg-cover h-full w-full rounded-3xl'>
+                <WeatherIcon className='w-16 h-16' imageUrl={imageUrl}/>
                 <Name value={name} />
                 <TemperatureRange value={temperatureRange} className='text-xs'/>
             </div>
