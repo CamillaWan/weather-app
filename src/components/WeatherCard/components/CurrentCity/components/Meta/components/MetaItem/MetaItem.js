4cacd1b5843271695fdcf5a3e821aad32c0898c1
CamillaWan <camillawan99@gmail.com>
2025-04-27 02:54:47 +1000

    fix: correct text size for MetaItem component in responsive layout

--- src/components/WeatherCard/components/CurrentCity/components/Meta/components/MetaItem/MetaItem.js
+++ src/components/WeatherCard/components/CurrentCity/components/Meta/components/MetaItem/MetaItem.js
@@ -2,7 +2,7 @@ import Image from '../../../Image'
 
 const MetaItem = ({imageUrl, alt, value}) => {
     return ( 
-        <div className='flex flex-col justify-between w-1/4 h-full items-center text-center p-1 text-xs md:text-xxxs lg:text-xxs xl:text-sm whitespace-nowrap'>
+        <div className='flex flex-col justify-between w-1/4 h-full items-center text-center p-1 text-xs md:text-xxxs lg:text-xxs xl:text-xs whitespace-nowrap'>
             <Image imageUrl={imageUrl} alt={alt} className='m-2'/>
             {value}
         </div>
