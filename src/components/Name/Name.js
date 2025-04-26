5eff7a0c8e12c2a1482513af09efcb93f4c46dd6
CamillaWan <camillawan99@gmail.com>
2025-04-27 01:56:30 +1000

    fix: enhance responsiveness and layout of components, add new font sizes for better text scaling

--- src/components/Name/Name.js
+++ src/components/Name/Name.js
@@ -1,5 +1,5 @@
 const Name = ({value,className}) => (
-    <div className= {`${className} flex justify-center items-center text-center`}>
+    <div className= {`${className} flex justify-center items-center text-center whitespace-nowrap`}>
         {value}
     </div>
 );
--- src/components/WeatherCard/components/OtherCities/Components/City/City.js
+++ src/components/WeatherCard/components/OtherCities/Components/City/City.js
@@ -4,11 +4,11 @@ import WeatherIcon from "../../../../../WeatherIcon";
 
 const City = ({className, imageUrl, name, temperatureRange}) => {
     return ( 
-        <div className={`${className} h-full w-full bg-cover bg-no-repeat bg-center mx-2 rounded-3xl flex flex-col justify-center items-center shadow-lg`}>
+        <div className={`${className} w-full bg-cover bg-no-repeat bg-center mx-2 rounded-3xl flex flex-col justify-center items-center shadow-lg`}>
             <div className='flex flex-col items-center justify-center overflow-hidden bg-gradient-to-l opacity-70 bg-cover h-full w-full rounded-3xl'>
-                <WeatherIcon className='w-16 h-16' imageUrl={imageUrl}/>
-                <Name value={name} />
-                <TemperatureRange value={temperatureRange} className='text-xs'/>
+                <WeatherIcon className='w-1/2' imageUrl={imageUrl}/>
+                <Name value={name} className='text-xs lg:text-sm xl:text-base'/>
+                <TemperatureRange value={temperatureRange} className='text-xxs lg:text-xs xl:text-s'/>
             </div>
         </div>
      );
