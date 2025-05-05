b3080d633c8f81d557a4f0039bd992d7eab13bd6
CamillaWan <camillawan99@gmail.com>
2025-05-06 04:39:50 +1000

    fix: update API call to exclude daily data and log other cities weather data

--- src/components/WeatherCard/components/OtherCities/Components/City/City.js
+++ src/components/WeatherCard/components/OtherCities/Components/City/City.js
@@ -4,7 +4,7 @@ import WeatherIcon from "../../../../../WeatherIcon";
 
 const City = ({className, imageUrl, name, temperatureRange}) => {
     return ( 
-        <div className={`${className} w-full bg-cover bg-no-repeat bg-center mx-2 rounded-3xl flex flex-col justify-center items-center`}>
+        <div className={`${className} opacity-100 w-full bg-cover bg-no-repeat bg-center mx-2 rounded-3xl flex flex-col justify-center items-center`}>
             <div className='flex flex-col items-center justify-center overflow-hidden bg-gradient-to-l opacity-70 hover:opacity-100 bg-cover h-full w-full rounded-3xl'>
                 <WeatherIcon className='w-1/2' imageUrl={imageUrl}/>
                 <Name value={name} className='text-xs lg:text-sm xl:text-base'/>
