7bf9cb4580ff486abba6f596b3b7de488295a1aa
CamillaWan <camillawan99@gmail.com>
2025-06-19 16:44:04 +1000

    fix(ui): adjust HeaderBar styles for consistency and responsiveness

--- src/components/HeaderBar/HeaderBar.js
+++ src/components/HeaderBar/HeaderBar.js
@@ -2,23 +2,25 @@ const HeaderBar = ({ user, showAuth, onLoginClick, onLogout, onCancel }) => {
   const name = user?.user_metadata?.name || user?.email || "User";
 
   return (
-    <header className="w-full flex justify-between items-center px-6 py-3 bg-blue-600 text-white shadow">
-      <h1 className="text-lg font-bold tracking-wide">Camilla's Weather App</h1>
+    <header className="w-full flex justify-between items-center px-6 py-3 text-white shadow">
+      <h1 className=" text-base md:text-lg font-bold tracking-wide">
+        Camilla's Weather App
+      </h1>
 
       <div>
         {showAuth ? (
           <button
             onClick={onCancel}
-            className="text-white px-4 py-1 rounded hover:underline"
+            className="text-white  px-4 py-1 rounded hover:underline"
           >
             Back
           </button>
         ) : user ? (
-          <div className="flex items-center space-x-4">
+          <div className="flex items-center text-sm md:text-base space-x-4">
             <span>Welcome, {name}</span>
             <button
               onClick={onLogout}
-              className="text-white px-3 py-1 rounded hover:underline"
+              className="text-white text-sm md:text-base px-3 py-1 rounded hover:underline"
             >
               Logout
             </button>
