449fe2fc7201a1bcaf01463e451d38cd4181d1ca
CamillaWan <camillawan99@gmail.com>
2025-08-10 03:43:32 +1000

    fix: move title attribute to Image component in MetaItem for improved accessibility

--- src/components/WeatherCard/components/CurrentCity/components/Meta/components/MetaItem/MetaItem.js
+++ src/components/WeatherCard/components/CurrentCity/components/Meta/components/MetaItem/MetaItem.js
@@ -4,10 +4,9 @@ const MetaItem = ({ imageUrl, alt, value }) => {
   return (
     <div
       aria-label={alt}
-      title={alt}
       className="flex flex-col justify-between w-1/4 h-full items-center text-center p-1 text-s md:text-xxs lg:text-s xl:text-base whitespace-nowrap"
     >
-      <Image imageUrl={imageUrl} alt={alt} className="m-2" />
+      <Image imageUrl={imageUrl} title={alt} alt={alt} className="m-2" />
       {value}
     </div>
   );
